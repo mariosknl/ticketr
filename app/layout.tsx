368e732fdbabbb33d0ecdb08165ad03f4ea36db5
mariosknl <mariosknl@outlook.com>
2024-11-22 13:26:07 +0200

    Refactor layout and add SyncUserWithConvex component, backend starting point

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ConvexClientProvider } from "@/components/ConvexClientProvider";
 import Header from "@/components/Header";
+import SyncUserWithConvex from "@/components/SyncUserWithConvex";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -34,6 +35,7 @@ export default function RootLayout({
         <ConvexClientProvider>
           <ClerkProvider>
             <Header />
+            <SyncUserWithConvex />
             {children}
           </ClerkProvider>
         </ConvexClientProvider>
